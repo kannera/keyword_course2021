54b21400b9f7c4c91d84cb75c68b16df47b78cfe
Antti Kanner <antti.kanner@helsinki.fi>
2022-01-31 13:07:18 +0200

    Update eng_corpus.py

--- eng_corpus.py
+++ eng_corpus.py
@@ -2,7 +2,7 @@ import wget
 import pandas
 import codecs
 
-def get_data_from_github(wget, corpus):
+def get_data_from_github(corpus):
   if corpus == "coha":
     file_names = ["fic_1.txt","fic_2.txt","mag_1.txt","mag_2.txt","news_1.txt","news_2.txt","nf_1.txt","nf_2.txt"]
     root_path = "https://raw.githubusercontent.com/kannera/keyword_course2021/main/coha/"
