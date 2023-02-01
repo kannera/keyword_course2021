a642a2dad0ec592feb435d4e217e73f9788db23f
Antti Kanner <antti.kanner@helsinki.fi>
2023-02-01 09:50:47 +0200

    Update korp_api.py

--- korp_api.py
+++ korp_api.py
@@ -184,7 +184,8 @@ def query_frequencies(query, groupby, corpus, allfr=False):
   return url
 
 def download(url):
-  tmp = wget.download(url, out="tmp.json")
+  os.system("wget --load-cookies=cookies.txt "+'"'+url+'"'+ " -O tmp.json")
+  #tmp = wget.download(url, out="tmp.json")
   with codecs.open("tmp.json", "r", encoding="utf-8", errors="replace") as f:
     data = json.load(f)
   #os.remove("tmp.json")
