654b638ff5a497afdce7e0acee46940a31910353
Antti Kanner <antti.kanner@helsinki.fi>
2023-02-14 10:53:03 +0200

    Update korp_api.py

--- korp_api.py
+++ korp_api.py
@@ -184,15 +184,15 @@ def query_frequencies(query, groupby, corpus, allfr=False):
   return url
 
 def download(url):
-  with open("cookies.txt", "r", encoding="utf-8") as f:
-    print([x for x in f])
-  os.system('wget --load-cookies=cookies.txt "'+url+'" -O tmp.json')
-  #tmp = wget.download(url, out="tmp.json")
+  #with open("cookies.txt", "r", encoding="utf-8") as f:
+  #  print([x for x in f])
+  #os.system('wget --load-cookies=cookies.txt "'+url+'" -O tmp.json')
+  tmp = wget.download(url, out="tmp.json")
   with codecs.open("tmp.json", "r", encoding="utf-8", errors="replace") as f:
     data = json.load(f)
   print(data)
   #os.remove("tmp.json")
-  #os.remove(tmp)
+  os.remove(tmp)
   return data
 
 def parse_date(x):
